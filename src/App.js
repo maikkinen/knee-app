7eceb5db0d69910ab8c3b6fd714f4b4ad54d0eff
unknown <anna-maija.rauramaa@aalto.fi>
2021-02-26 14:30:16 +0100

    MVP layout created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import KneeFront from './assets/KneeFront.svg'
+import './App.css'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="Content-wrapper-main">
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <img src={KneeFront} className="Knee" alt="knee" />
+      <div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit our Website
         </a>
-      </header>
+      </div>
     </div>
   );
 }

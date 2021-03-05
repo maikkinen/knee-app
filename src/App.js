e47f2c95c8f0fdc9df078aeaa91e923acc2d5b8f
unknown <anna-maija.rauramaa@aalto.fi>
2021-03-05 15:10:37 +0100

    svg document's sub elements fully accessible in the dom

--- src/App.js
+++ src/App.js
@@ -1,79 +1,86 @@
-import KneeFront from './assets/KneeFront.svg'
 import './App.css'
+import { useState } from 'react'
 import { gsap } from 'gsap'
 import { TimelineLite } from 'gsap/all'
+import Knee from './Knee.js'
 
 const App = () => {
+
   return (
     <div className="Content-wrapper-main">
-      <img id="knee" src={KneeFront} className="Knee" alt="knee" />
+      <span className="Knee" id="knee">
+        <Knee />
+      </span>
       <div className="Welcoming-text">
         <a className="App-link" href="/" rel="noopener noreferrer">Visit our Website</a>
-        <button onClick={() => rotate()} className="Button-primary">Animate</button>
+        <button onClick={() => flexingQuadriceps()} className="Button-primary">Flex some Muscle</button>
+        <button onClick={() => kneeAnimationMaster()} className="Button-primary">Show some Animation</button>
       </div>
     </div>
   );
 }
 
-const rotate = () => {
-  console.log(1)
-  var muscle = document.querySelector("#knee #Quadriceps")
-  console.log(2)
-  console.log("muscle", muscle)
-
-  var tl = new TimelineLite({});
-  tl.to(muscle, {
-    scale: 2,
-    rotate: 2,
-    duration: 2
-  })
-  console.log(3)
+const kneeAnimationMaster = () => {
+  console.log('here')
+  wiggleTibia()
+  //bounce()
 }
 
-const wiggle = () => {
-  /*   gsap.from("#knee", {
-      scale: 1,
-      rotate: 0,
+
+const flexingQuadriceps = () => {
+  var Quadriceps = document.querySelector("#Quadriceps")
+  var tl = new TimelineLite({});
+  tl.to(Quadriceps, {
+    scaleX: 0.95,
+    duration: 1
+  })
+
+    .to(Quadriceps, {
+      scaleX: 1.05,
+      duration: 0.5
     })
-    gsap.to("#knee", {
-      scale: 1.1,
-      rotate: 5,
-      duration: 3,
-      ease: 'elastic'
-    }) */
+    .to(Quadriceps, {
+      scaleX: 1,
+      duration: 1.75
+    })
+}
+
+const wiggleTibia = () => {
+  var Tibia = document.querySelector("#Tibia")
+
   gsap.timeline({
-    repeat: -1,
+    //repeat: -1,
     defaults: { ease: 'elastic' }
   })
     .add('start cycle')
-    .to("#knee", {
+    .to(Tibia, {
       scale: 1.02,
-      rotate: 1.2,
-      transformOrigin: 'bottom right 50px',
-      duration: 4.5,
+      rotate: 1.05,
+      transformOrigin: 'bottom right 20px',
+      duration: 2.5,
     })
-    .to("#knee", {
+    .to(Tibia, {
       scale: 0.98,
-      rotate: -1.15,
+      rotate: 0,
       duration: 3.5,
     })
 }
 
 const bounce = () => {
-  var tl = new TimelineLite( {
+  var tl = new TimelineLite({
     repeat: -1
   });
 
   tl.add('bounce')
     .to('#knee', { y: '1rem', ease: "elastic.config(1.75, 0.3)", duration: 2.5 })
-    .to('#knee', { y: '0', ease: "elastic.config(1.75, 0.3)", duration: 4})
+    .to('#knee', { y: '0', ease: "elastic.config(1.75, 0.3)", duration: 4 })
 }
 
 
 
 window.onload = () => {
-  //wiggle()
-  bounce()
+  //wiggleTibia()
+  //bounce()
 }
 /* 
 Comment out some code? 
